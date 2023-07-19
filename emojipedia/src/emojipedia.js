a323a71d70ebc54e4d68e617dbe903189478943d
Roman <romandngl121@gmail.com>
2023-07-19 16:00:42 +0545

    Initial Commit

---
+++ emojipedia/src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "❤️",
+    name: "Red Heart",
+    meaning:
+      "A classic red love heart emoji, used for expressions of love and romance.",
+  },
+  {
+    id: 5,
+    emoji: "😙 ",
+    name: "Kissing Face with Smiling Eyes",
+    meaning:
+      "A yellow face with smiling eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection.",
+  },
+  {
+    id: 6,
+    emoji: "😭",
+    name: "Loudly Crying Face",
+    meaning:
+      "A yellow face with an open mouth wailing and streams of heavy tears flowing from closed eyes. May convey inconsolable grief but also other intense feelings, such as uncontrollable laughter, pride or overwhelming joy.",
+  },
+];
+
+export default emojipedia;
